2125ea8d318497c34f72c63a61f8c40e12419127
rajatrjsharma <merajatsharma@outlook.com>
2023-03-10 18:19:27 +0530

    Added the Home as next component to App and all components from Home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import ContactMe from "./PortfolioContainer/ContactMe/ContactMe";
-import Footer from "./PortfolioContainer/Footer/Footer";
 import Home from "./PortfolioContainer/Home/Home";
-import Resume from "./PortfolioContainer/Resume/Resume";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <Resume />
-      <ContactMe />
-      <Footer />
     </div>
   );
 }
--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import Profile from "./Profile/Profile";
 import ProfileFooter from "./ProfileFooter/ProfileFooter";
+import ContactMe from "../ContactMe/ContactMe";
+import Footer from "../Footer/Footer";
+import Resume from "../Resume/Resume";
 import "./Home.css";
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="home-container">
-        <Profile />
-        <ProfileFooter />
-      </div>
+      <>
+        <div className="home-container">
+          <Profile />
+          <ProfileFooter />
+        </div>
+        <Resume />
+        <ContactMe />
+        <Footer />
+      </>
     );
   }
 }
